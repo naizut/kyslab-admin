020776ce28ea8b39750fb171a60af272addb349a
周文恺 <naizut@163.com>
2021-12-29 18:00:21 +0800

    引入AXIOS

--- src/pages/Article/Articles.tsx
+++ src/pages/Article/Articles.tsx
@@ -1,9 +1,18 @@
 import { Component } from 'react'
+import axios from 'axios'
 
 class Articles extends Component {
+  componentDidMount() {
+    axios.get('/api/articles/get?id=19', {}).then(res => {
+      console.log(res)
+    }).catch(error => {
+        console.error(error)
+    })
+  }
+
   render() {
-    return <div className="Articles">
-      Articles
+    return <div className="admin-article">
+      <div></div>
     </div>
   }
 }
